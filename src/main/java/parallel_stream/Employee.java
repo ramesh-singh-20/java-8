84eca0e644d685c717fb2fe96c47fb6e06481e50
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-01-19 08:27:09 -0500

    Implemented parallel streams  with Stream API.

---
+++ src/main/java/parallel_stream/Employee.java
@@ -0,0 +1,17 @@
+package parallel_stream;
+
+public class Employee {
+    private Double salary;
+
+    public Employee(Double salary){
+        this.salary= salary;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
--- src/main/java/parallel_stream/ParallelStreamExample.java
+++ src/main/java/parallel_stream/ParallelStreamExample.java
@@ -1,11 +1,14 @@
 package parallel_stream;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 public class ParallelStreamExample {
 
     public static void parallelStream(){
-        long startTime= System.currentTimeMillis();
+        /*long startTime= System.currentTimeMillis();
         IntStream.range(1, 100).forEach(System.out:: println);
         long endTime= System.currentTimeMillis();
         System.out.println("Time Taken by Sequential Stream: "+(endTime-startTime));
@@ -14,7 +17,38 @@ public class ParallelStreamExample {
         long startTime1= System.currentTimeMillis();
         IntStream.range(1, 100).parallel().forEach(System.out:: println);
         long endTime1= System.currentTimeMillis();
-        System.out.println("Time Taken by parallel Stream: "+(endTime1-startTime1));
+        System.out.println("Time Taken by parallel Stream: "+(endTime1-startTime1));*/
+
+        List<Employee> employeeList= new ArrayList<>();
+        for(int i=0; i< 1000; i++){
+            Employee employee= new Employee(new Random().nextDouble()*1000);
+            employeeList.add(employee);
+        }
+
+        long start= 0;
+        long end= 0;
+
+        start= System.currentTimeMillis();
+        var salarySeqStream = employeeList.stream()
+                .map(emp -> emp.getSalary())
+                .mapToDouble(i -> i)
+                .average().getAsDouble();
+
+        end= System.currentTimeMillis();
+        System.out.println("Time taken by sequential stream: "+ (end-start)
+        + " Avg salary: "+ salarySeqStream);
+
+        start= System.currentTimeMillis();
+        var salaryParallelStream = employeeList.parallelStream()
+                .map(emp -> emp.getSalary())
+                .mapToDouble(i -> i)
+                .average().getAsDouble();
+
+        end= System.currentTimeMillis();
+        System.out.println("Time taken by parallel stream: "+ (end-start)
+                + " Avg salary: "+ salaryParallelStream);
+
+
     }
 
 
